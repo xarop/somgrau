66802e38105091c5da12e09fdc05661286b7f344
xarop <ajl@xarop.com>
2026-03-31 18:04:42 +0200

    Refactor functions.php and format theme.json for improved readability

--- wp-content/themes/somgrau/functions.php
+++ wp-content/themes/somgrau/functions.php
@@ -1,6 +1,8 @@
 <?php
 // SomGrau child theme functions
-add_action( 'wp_enqueue_scripts', function() {
-    wp_enqueue_style( 'somgrau-style', get_stylesheet_uri(), [], wp_get_theme()->get('Version') );
-} );
+add_action(
+    'wp_enqueue_scripts', function () {
+        wp_enqueue_style('somgrau-style', get_stylesheet_uri(), [], wp_get_theme()->get('Version'));
+    } 
+);
 require_once get_stylesheet_directory() . '/xarop.php';
